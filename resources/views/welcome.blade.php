09565cfed986e5967ffc308d964bd17afd6490cf
Talal Jamil <Example@gmail.com>
2024-04-01 12:12:17 +0500

    initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -169,6 +169,7 @@
                 nearestLocations = [
 
                     // {
+                    // MAP_API_KEY=AIzaSyA_L-LDpdgp2QRNyRsWM8dfDta8lgfEqvU
                     //     lat = '31.4828768',
                     //     lng = '74.2596828',
                     //     postCode = '54600',
