fd40abb837a0f0ad751f173c044bf75fe917b068
Talal Jamil <Example@gmail.com>
2024-04-01 12:26:41 +0500

    initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -172,7 +172,7 @@
                     //     lat = '31.4828768',
                     //     lng = '74.2596828',
                     //     postCode = '54600',
-                    //     areaName = 'Model Town, Lahore, Punjab AIzaSyA_L-LDpdgp2QRNyRsWM8dfDta8lgfEqvU',
+                    //     areaName = ' AIzaSyA_L-LDpdgp2QRNyRsWM8dfDta8lgfEqvU Model Town, Lahore, Punjab ',
 
                     // },
                     // {
